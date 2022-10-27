4a2122e886b9259b48d745d73275ac04bb825273
Alumno Administración <aadmon@utags.edu.mx>
2022-10-26 19:22:47 -0500

    First commit

---
+++ callstack.js
@@ -0,0 +1,13 @@
+function saludar() {
+    // [1] Código
+    diHola();
+    // [2] Código
+ }
+ function diHola() {
+    return "!Hola!";
+ }
+ 
+ // Invocar la función `saludar`
+ saludar();
+ 
+ // [3] Código
